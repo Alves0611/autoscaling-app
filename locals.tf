82ef9fedbb8e85c0f7ca29e535dd5fdbefa5f141
Alves0611 <g.alves0611@gmail.com>
2024-08-27 20:39:16 -0300

    feat: add local subnets configuration with CIDR blocks, availability zones, and names

--- locals.tf
+++ locals.tf
@@ -1,6 +1,43 @@
 locals {
   namespaced_service_name = "${var.service_name}-${var.environment}"
 
+  subnets = {
+    "pub_a" = {
+      cidr_block = "10.0.0.0/20"
+      az         = "a"
+      name       = "public-a"
+    }
+    "pub_b" = {
+      cidr_block = "10.0.16.0/20"
+      az         = "b"
+      name       = "public-b"
+    }
+    "pub_c" = {
+      cidr_block = "10.0.32.0/20"
+      az         = "b"
+      name       = "public-c"
+    }
+    "pvt_a" = {
+      cidr_block = "10.0.128.0/20"
+      az         = "a"
+      name       = "private-a"
+    }
+    "pvt_b" = {
+      cidr_block = "10.0.144.0/20"
+      az         = "b"
+      name       = "private-b"
+    }
+    "pvt_c" = {
+      cidr_block = "10.0.160.0/20"
+      az         = "b"
+      name       = "private-c"
+    }
+  }
+  subnet_ids = {
+    for k, v in aws_subnet.this : v.tags.Name => v.id
+  }
+  public_subnet_ids   = [aws_subnet.this["pub_a"].id, aws_subnet.this["pub_b"].id]
+  internet_cidr_block = "0.0.0.0/0"
   common_tags = {
     Component = "auto-scalable"
     ManagedBy = "Terraform"
