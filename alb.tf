c4b3f4ef503ac65fb4392670bbec349944fb30b3
Alves0611 <g.alves0611@gmail.com>
2024-08-27 20:56:50 -0300

    feat: add ALB target group with HTTP protocol and health check configuration

--- alb.tf
+++ alb.tf
@@ -9,3 +9,22 @@ resource "aws_alb" "this" {
     "Name" = local.namespaced_service_name
   }
 }
+
+resource "aws_alb_target_group" "http" {
+  name     = local.namespaced_service_name
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.this.id
+
+  health_check {
+    enabled             = var.alb_healthcheck_config.enabled
+    healthy_threshold   = var.alb_healthcheck_config.healthy_threshold
+    interval            = var.alb_healthcheck_config.interval
+    matcher             = var.alb_healthcheck_config.matcher
+    path                = var.alb_healthcheck_config.path
+    port                = var.alb_healthcheck_config.port
+    protocol            = var.alb_healthcheck_config.protocol
+    timeout             = var.alb_healthcheck_config.timeout
+    unhealthy_threshold = var.alb_healthcheck_config.unhealthy_threshold
+  }
+}
