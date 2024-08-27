ff3b816eaaaa92a34d98e57c7968a2437b6c0502
Alves0611 <g.alves0611@gmail.com>
2024-08-27 20:34:18 -0300

    feat: create AWS Internet Gateway and Subnet resources with namespaced tags

--- vpc.tf
+++ vpc.tf
@@ -4,3 +4,22 @@ resource "aws_vpc" "this" {
     "Name" = local.namespaced_service_name
   }
 }
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+  tags = {
+    "Name" = local.namespaced_service_name
+  }
+}
+
+resource "aws_subnet" "this" {
+  for_each = local.subnets
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = each.value.cidr_block
+  availability_zone = "${var.aws_region}${each.value.az}"
+
+  tags = {
+    "Name" = "${local.namespaced_service_name}-${each.value.name}"
+  }
+}
